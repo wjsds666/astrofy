187a0b5bfa2ef8059a7f74a73144df0e3adde5ab
Wang Jiashuo <2784672381@qq.com>
2026-05-02 13:23:37 +0800

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'https://wjsds666.github.io',
+  base: '/astrofy', 
   integrations: [mdx(), sitemap(), tailwind()]
 });
